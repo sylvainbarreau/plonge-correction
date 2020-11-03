fb43ff547ab0e2fb2477dff5440f74f18e88d087
Rémi Bastide <Remi.Bastide@gmail.com>
2020-11-03 16:21:09 +0100

    First Commit

---
+++ src/diningphilosophers/ChopStick.java
@@ -0,0 +1,35 @@
+package diningphilosophers;
+
+public class ChopStick {
+
+    private static int stickCount = 0;
+
+    private boolean iAmFree = true;
+    private final int myNumber;
+
+    public ChopStick() {
+        myNumber = ++stickCount;
+    }
+
+    synchronized public void take() throws InterruptedException {
+        while (!iAmFree) {
+            wait();
+        }
+        // assert iAmFree;
+        iAmFree = false;
+        System.out.println("Stick " + myNumber + " Taken");
+        // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
+    }
+
+    synchronized public void release() {
+        // assert !iAmFree;
+        System.out.println("Stick " + myNumber + " Released");
+        iAmFree = true;
+        notifyAll(); // On prévient ceux qui attendent que la baguette soit libre
+    }
+
+    @Override
+    public String toString() {
+        return "Stick#" + myNumber;
+    }
+}
---
+++ src/diningphilosophers/Philosopher.java
@@ -0,0 +1,62 @@
+package diningphilosophers;
+
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Philosopher
+        extends Thread {
+
+    private static int seed = 1;
+    private final Random myRandom = new Random(System.currentTimeMillis() + seed++);
+    private final static int DELAY = 1000;
+    private final ChopStick myLeftStick;
+    private final ChopStick myRightStick;
+    private boolean running = true;
+
+    public Philosopher(String name, ChopStick left, ChopStick right) {
+        super(name);
+        myLeftStick = left;
+        myRightStick = right;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+                think();
+                myLeftStick.take();
+                think(); // Pour augmenter la probabilité d'interblocage
+                myRightStick.take();
+                // success : process
+                eat();
+                // release resources
+                myLeftStick.release();
+                myRightStick.release();
+                // try again
+            } catch (InterruptedException ex) {
+                Logger.getLogger("Table").log(Level.SEVERE, "{0} Interrupted", this.getName());
+            }
+        }
+    }
+
+    // Permet d'interrompre le philosophe "proprement" :
+    // Il relachera ses baguettes avant de s'arrêter
+    public void leaveTable() {
+        running = false;
+    }
+
+    private void think() throws InterruptedException {
+        int delay = myRandom.nextInt(500 + DELAY);
+        System.out.println(this.getName() + " Starts Thinking for: " + delay + " ms");
+        sleep(delay); // Le thread peut être interrompu ici
+        System.out.println(this.getName() + " Stops Thinking");
+    }
+
+    private void eat() throws InterruptedException {
+        int delay = myRandom.nextInt(100 + DELAY);
+        System.out.println(this.getName() + " Starts Eating for:" + delay + " ms");
+        sleep(delay); // Le thread peut être interrompu ici
+        System.out.println(this.getName() + " Stops Eating");
+    }
+}
